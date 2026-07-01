e96adc6779ca688b5e0bb2383a01c8080da42c1a
iq.iaes <iq.iaes@gmail.com>
2026-07-01 10:54:08 +0700

    update tombol

--- resources/views/journals/create.blade.php
+++ resources/views/journals/create.blade.php
@@ -58,10 +58,10 @@
                     </div>
 
                     <div class="flex items-center justify-end space-x-3">
-                        <a href="{{ route('journals.index') }}" class="inline-flex items-center px-4 py-2 bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-gray-700 uppercase tracking-widest hover:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2">
+                        <a href="{{ route('journals.index') }}" class="inline-flex items-center px-4 py-2 bg-gray-500 text-white rounded">
                             Batal
                         </a>
-                        <button type="submit" class="inline-flex items-center px-4 py-2 bg-indigo-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+                        <button type="submit" class="inline-flex items-center px-4 py-2 bg-red-600 text-white rounded">
                             Simpan
                         </button>
                     </div>
